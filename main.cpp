2b1e89bbf682de403bbb09d41ffb4cbc06704731
Sofiavisconti03 <128122122+Sofiavisconti03@users.noreply.github.com>
2023-04-28 11:02:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n;
+    int n1=0;
+    int n2=1;
+    int numero_successivo;
+        cout << "inserire quanti numeri si vogliono visulizzare" << endl;
+    cin>>n;
+        if(n<3)
+            cout << "errore"<<endl;
+        else {
+        for (int i=1;i<=n;++i) {
+            if (i==1) {
+                cout << n1<<endl;
+                continue;
+            }
+            if (i==2) {
+                cout << n2<<endl;
+                continue;
+            }
+            n1 = n2;
+            numero_successivo= n1+n2;
+            n2 = numero_successivo;
+            cout << numero_successivo << ""<<""<<endl;
+        }}
+            return 0;
 }
